7d4378552230f20b4ff2c7fce88aa6294a25959c
Maarten Albrecht <maarten.albrecht@duo.nl>
2025-08-06 10:43:17 +0200

    add iframe functionality and script call in HelloWorld component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,32 @@
 
 
+
 <template>
-  <iframe src="/hello.html" width="100%" height="400" style="border:none;"></iframe>
+  <div>
+    <iframe ref="helloFrame" src="/hello.html" width="100%" height="400" style="border:none;"></iframe>
+    <button @click="callIframeScript">Call iframe script</button>
+    <div v-if="iframeResult">Result: {{ iframeResult }}</div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      iframeResult: ''
+    }
+  },
+  methods: {
+    callIframeScript() {
+      const iframe = this.$refs.helloFrame;
+      if (iframe && iframe.contentWindow && iframe.contentWindow.getHelloMessage) {
+        this.iframeResult = iframe.contentWindow.getHelloMessage();
+      } else {
+        this.iframeResult = 'Function not available yet.';
+      }
+    }
+  }
+}
+</script>
 
 
