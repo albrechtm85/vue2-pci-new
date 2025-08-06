c3f9702fe89b4678abb77da8ab89c1352e72a20e
Maarten Albrecht <maarten.albrecht@duo.nl>
2025-08-06 11:43:25 +0200

    enhance iframe communication by implementing API verification and call handling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,53 +3,40 @@
 
 <template>
   <div>
-    <iframe ref="helloFrame" src="/hello.html" width="100%" height="400" style="border:none;"></iframe>
+    <iframe ref="helloFrame" src="/hello.html" width="100%" height="400" style="border:none;" sandbox="allow-scripts"></iframe>
     <button @click="callIframeScript">Call iframe script</button>
     <div v-if="iframeResult">Result: {{ iframeResult }}</div>
   </div>
 </template>
 
 <script>
-import iframeBridge from '../iframeBridge';
+import IframeBridge from '../iframeBridge';
 
 export default {
   data() {
     return {
       iframeResult: '',
-      iframeContext: null,
+      bridge: null,
       apiVerified: false
     }
   },
   mounted() {
-    this.setIframeContext();
+    const iframe = this.$refs.helloFrame;
+    iframe.addEventListener('load', () => {
+      this.bridge = new IframeBridge(iframe);
+      this.bridge.verifyApi().then((verified) => {
+        this.apiVerified = verified;
+      });
+    });
   },
   methods: {
-    setIframeContext() {
-      const iframe = this.$refs.helloFrame;
-      const setContext = () => {
-        const ctx = iframe.contentWindow.iframeContext;
-        if (iframeBridge.verifyApi(ctx)) {
-          this.iframeContext = ctx;
-          this.apiVerified = true;
-        } else {
-          this.apiVerified = false;
-        }
-      };
-      if (iframe && iframe.contentWindow) {
-        if (iframe.contentWindow.iframeContext) {
-          setContext();
-        } else {
-          iframe.addEventListener('load', setContext);
-        }
-      }
-    },
     callIframeScript() {
-      if (this.apiVerified) {
-        try {
-          this.iframeResult = iframeBridge.call(this.iframeContext, 'getHelloMessage');
-        } catch (e) {
-          this.iframeResult = e.message;
-        }
+      if (this.apiVerified && this.bridge) {
+        this.bridge.call('getHelloMessage').then(result => {
+          this.iframeResult = result;
+        }).catch(err => {
+          this.iframeResult = err.message;
+        });
       } else {
         this.iframeResult = 'Iframe API not verified.';
       }
--- src/iframeBridge.js
+++ src/iframeBridge.js
@@ -2,15 +2,55 @@
 
 const REQUIRED_API = ['getHelloMessage', 'getTime'];
 
-export default {
-  verifyApi(context) {
-    if (!context) return false;
-    return REQUIRED_API.every(fn => typeof context[fn] === 'function');
-  },
-  call(context, fnName, ...args) {
-    if (!context || typeof context[fnName] !== 'function') {
-      throw new Error(`Function ${fnName} not available in iframe context.`);
-    }
-    return context[fnName](...args);
+class IframeBridge {
+  constructor(iframe) {
+    this.iframe = iframe;
+    this.apiVerified = false;
+    this.apiCheckDone = false;
+    this.pending = {};
+    window.addEventListener('message', this._handleMessage.bind(this));
   }
-};
+
+  verifyApi() {
+    return new Promise((resolve) => {
+      this._send('verifyApi', { required: REQUIRED_API });
+      this._once('apiVerified', (payload) => {
+        console.log('API verification result:', payload);
+        
+        this.apiVerified = payload;
+        this.apiCheckDone = true;
+        resolve(payload);
+      });
+    });
+  }
+
+  call(fnName, ...args) {
+    return new Promise((resolve, reject) => {
+      if (!this.apiCheckDone || !this.apiVerified) {
+        return reject(new Error('Iframe API not verified.'));
+      }
+      this._send('call', { fnName, args });
+      this._once('callResult', (result) => {
+        resolve(result);
+      });
+    });
+  }
+
+  _send(type, payload = {}) {
+    this.iframe.contentWindow.postMessage({ type, ...payload }, '*');
+  }
+
+  _once(type, cb) {
+    this.pending[type] = cb;
+  }
+
+  _handleMessage(event) {
+    if (!event.data || !event.data.type) return;
+    if (this.pending[event.data.type]) {
+      this.pending[event.data.type](event.data.result);
+      delete this.pending[event.data.type];
+    }
+  }
+}
+
+export default IframeBridge;
